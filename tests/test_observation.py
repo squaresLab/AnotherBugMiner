c7a430df964ee1aca14b2c0b2bd5a10ec7e34621
ChrisTimperley <christimperley@googlemail.com>
2018-03-09 19:40:35 -0500

    tests: added test_save

--- tests/test_observation.py
+++ tests/test_observation.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 
 import pytest
 
@@ -25,10 +26,7 @@ def test_properties():
     assert observation.build_url_fix == build_url_fix
 
 
-def test_load():
-    fn = os.path.join(os.path.dirname(__file__), 'observation/bugs.csv')
-    observations = ObservationCollection.load(fn)
-
+def check_bugs_collection(observations: ObservationCollection):
     assert len(observations) == 1
 
     contents = list(observations)
@@ -38,3 +36,22 @@ def test_load():
     assert entry.build_url_bug == 'https://travis-ci.org/IQSS/dataverse/jobs/289573808'
     assert entry.commit_fix == '5a53fa0f42b88b614685ebfd72ad47428f1c2028'
     assert entry.build_url_fix == 'https://travis-ci.org/IQSS/dataverse/builds/289591312'
+
+
+def test_load():
+    fn = os.path.join(os.path.dirname(__file__), 'observation/bugs.csv')
+    observations = ObservationCollection.load(fn)
+    check_bugs_collection(observations)
+
+
+def test_save():
+    fn_original = os.path.join(os.path.dirname(__file__), 'observation/bugs.csv')
+    observations_original = ObservationCollection.load(fn_original)
+
+    fn_temp = tempfile.NamedTemporaryFile('w')
+    try:
+        observations_original.save(fn_temp.name)
+        observations_loaded = ObservationCollection.load(fn_temp.name)
+        check_bugs_collection(observations_loaded)
+    finally:
+        fn_temp.close()
