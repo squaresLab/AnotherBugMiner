d20fa3dba4c1622d6f5b0a3f53437bd3760cf51c
Jeremy Lacomis <jlacomis@cmu.edu>
2018-03-16 12:40:31 -0400

    Some skeleton stuff for github_commit_to_travis_build_url

--- src/abm/util.py
+++ src/abm/util.py
@@ -62,4 +62,12 @@ def github_commit_to_travis_build_url(repo: str, commit: str) -> str:
     Returns:
         The URL to the Travis build for the given commit.
     """
+    repo_slug = parse_github_url(repo)
+    repo = token.repo(repo_slug)
+
+    # From the TravisPy docs:
+    # "There is no API endpoint for resolving commits, however commit data
+    # might be included in other API entities, like Build or Job."
+    # I think this means that we have to do a dumb linear search of all of
+    # the builds in the repo to find the one that corresponds to this commit.
     raise NotImplementedError
